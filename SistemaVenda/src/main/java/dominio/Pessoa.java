bc0da410bf94967b91a6155501217f1bfe678fd2
Helton Ribeiro Nunes <heltonrn@gmail.com>
2022-05-24 20:11:39 -0300

    Projeto Inicial

---
+++ SistemaVenda/src/main/java/dominio/Pessoa.java
@@ -0,0 +1,34 @@
+package dominio;
+
+public class Pessoa {
+	
+	private Integer id;
+	private String nome;
+	public Pessoa() {
+		super();
+	}
+	public Pessoa(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+	
+
+}
